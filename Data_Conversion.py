79b1c72bd34dd73e4e62276c0e6f6e0b2430bd7f
Bryan-Wriggler <bryan.h.940707@gmail.com>
2024-01-14 00:06:46 -0800

    Successfully convert the data to fit Tenorflow, and change the directory

--- Data_Conversion.py
+++ Data_Conversion.py
@@ -1,29 +1,37 @@
 import tensorflow as tf
 
 def create_tf_example(image_path, label):
-    image = open(image_path, 'rb').read()  # Read the image binary data
+    # Read the image binary data
+    image = open(image_path, 'rb').read()
+
+    # Encode the label as bytes
+    label_bytes = label.encode('utf-8')
+
+    # Create a feature dictionary
     feature = {
         'image': tf.train.Feature(bytes_list=tf.train.BytesList(value=[image])),
-        'label': tf.train.Feature(int64_list=tf.train.Int64List(value=[label])),
+        'label': tf.train.Feature(bytes_list=tf.train.BytesList(value=[label_bytes])),
     }
+
+    # Create a TensorFlow Example object
     tf_example = tf.train.Example(features=tf.train.Features(feature=feature))
     return tf_example
 
+
 # Example usage
-tf_record_filename = 'Users/Desktop/Hand-Written-Note-Detection/Hand-Written-Note-Conversion/Data'
+tf_record_filename = 'DataForTF'
 
 #read the csv file to get the image paths and label
 import csv
 
-with open('Data/english.csv') as file:
-    data = csv.reader(file)
+data = csv.reader(open('english.csv'))
 
 
 with tf.io.TFRecordWriter(tf_record_filename) as writer:
-
     for row in data:
-        tf_example = create_tf_example('Data/Img/' + row[0], row[1])
-        writer.write(tf_example.SerializeToString())
+        if (row[0] != 'image'):
+            tf_example = create_tf_example(row[0], row[1])
+            writer.write(tf_example.SerializeToString())
 
 
 
